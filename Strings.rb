f91d6c93f35cbcc8bc5593101fe7360bfe8fc5dc
Joseph Gormley <josephbgormley@gmail.com>
2018-02-16 20:53:41 -0500

    Deleted Strings.rb, went with a different organization scheme.

--- Strings.rb
+++
@@ -1,7 +0,0 @@
-#!/usr/local/bin/ruby
-
-#The following program is based off a series of interviewing questions. 
-
-#1. Is Unique: Implement an algorithm to determine if a string has all unique characters. What if you cannot use additional data structures?
-
-
