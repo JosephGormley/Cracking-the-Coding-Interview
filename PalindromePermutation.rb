27d32b5e11664bd121703fbb63522e5c5d13bfe8
Joseph Gormley <josephbgormley@gmail.com>
2018-02-22 22:41:39 -0500

    Boilerplate code for problem 4.

---
+++ PalindromePermutation.rb
@@ -0,0 +1,19 @@
+#!/usr/local/bin/ruby
+
+#The following program is based off a series of interviewing questions.
+
+#4. Palindrome Permutation: Given a string, write a function to check if it is a permutation of a palindrome. A palindrome
+#   is a word or phrase that is the same forwards and backwards. A permutation is a rearrangement of letters. The palindrome 
+#   does not need to be limited to just dictionay words.
+# 
+#   EXAMPLE
+#
+#   Input:  "Tact Coa"
+#   Output: "True (permutations: "Taco cat", "atco cta", etc.)
+
+ #-----------#
+# Main Method #
+ #-----------#
+
+
+# End of Main
