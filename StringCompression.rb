2630a2f0cb8b125e9a0117697331deec73b6006a
Joseph Gormley <josephbgormley@gmail.com>
2018-02-26 22:09:28 -0500

    Added boilerplate code for StringCompression.rb

---
+++ StringCompression.rb
@@ -0,0 +1,19 @@
+#!/usr/local/bin/ruby
+
+#The following program is based off a series of interviewing questions.
+
+#6. String Compression: Implement a method to perform basic string compression using the counts of 
+#   repeated characters. For example, the string aabcccccaaa would become a2b1c5a3. If the 
+#   "compressed" string would not become smaller than the original string, your method should return 
+#   the original string. You can assume the string has uppercase and lowercasr letters (a-z). 
+
+
+
+
+ #-----------#
+# Main Method #
+ #-----------#
+
+
+   return
+# End of Main
